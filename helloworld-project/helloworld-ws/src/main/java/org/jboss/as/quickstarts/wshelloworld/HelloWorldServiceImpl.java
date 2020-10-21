498eefdbd07016a2917cced9d5f64a31a410b1ab
Alexey Afanasenko <xafonya@gmail.com>
2020-10-21 21:12:06 +0000

    add helloworld-project

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -33,6 +33,12 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public String sayHello() {
         return "Hello World!";
+
+
+
+
+
+        
     }
 
     @Override
