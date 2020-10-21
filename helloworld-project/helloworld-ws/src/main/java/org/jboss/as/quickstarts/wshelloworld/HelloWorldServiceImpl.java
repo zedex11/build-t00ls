0a5023c23347ccf40d74a230c28ff626576de1b9
SiarheiHryshchanka <zedex@tut.by>
2020-10-21 23:05:01 +0300

    loop

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -33,6 +33,7 @@ public class HelloWorldServiceImpl implements HelloWorldService {
     @Override
     public String sayHello() {
         return "Hello World!";
+        for (; ;) {}
     }
 
     @Override
