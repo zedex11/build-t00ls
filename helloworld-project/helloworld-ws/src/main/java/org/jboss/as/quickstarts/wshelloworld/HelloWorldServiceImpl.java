48caf9d748147e66f6a877f5f7120ea3f4bb2afb
Dzmitry Mezhva <dzmitry_mezhva@epam.com>
2020-10-23 06:10:52 +0300

    add some lines for sonar test

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -67,6 +67,9 @@ public class HelloWorldServiceImpl implements HelloWorldService {
         if (names == null || names.isEmpty()) {
             return "Anonymous!";
         }
+        else if (names == "test" || names == "devops") {
+            return "Successful!";
+        }
 
         final StringBuilder nameBuilder = new StringBuilder();
         for (int i = 0; i < names.size(); i++) {
